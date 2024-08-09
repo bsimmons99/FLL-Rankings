a63bf7f0d2218d0ba424ad2b0cc997ad70c6827a
Bryce Simmons <bryces99@bigpond.com>
2024-08-09 12:05:59 +0930

    Updated schema to support timer cache

--- schema.sql
+++ schema.sql
@@ -1,8 +1,27 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 09, 2024 at 12:00 PM
+-- Server version: 11.2.4-MariaDB-ubu2204
+-- PHP Version: 8.1.2-1ubuntu2.18
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-CREATE DATABASE IF NOT EXISTS `rankings` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `rankings`;
+
+--
+-- Database: `fllrankings`
+--
+CREATE DATABASE IF NOT EXISTS `fllrankings` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `fllrankings`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event`
+--
 
 CREATE TABLE `event` (
   `id` int(11) NOT NULL,
@@ -10,8 +29,15 @@ CREATE TABLE `event` (
   `rounds_practice` int(11) NOT NULL,
   `rounds_ranked` int(11) NOT NULL,
   `current_round` int(11) NOT NULL DEFAULT 1,
-  `stage` tinyint(1) NOT NULL DEFAULT 0
+  `stage` tinyint(1) NOT NULL DEFAULT 0,
+  `timer_start_cache` int(10) UNSIGNED NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `score`
+--
 
 CREATE TABLE `score` (
   `id` int(11) NOT NULL,
@@ -25,6 +51,12 @@ CREATE TABLE `score` (
   `round` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `team`
+--
+
 CREATE TABLE `team` (
   `id` int(11) NOT NULL,
   `event` int(11) NOT NULL,
@@ -34,52 +66,100 @@ CREATE TABLE `team` (
   `pit` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
 CREATE TABLE `user` (
   `event` int(11) NOT NULL,
   `name` int(11) NOT NULL,
   `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Indexes for dumped tables
+--
 
+--
+-- Indexes for table `event`
+--
 ALTER TABLE `event`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `score`
+--
 ALTER TABLE `score`
   ADD PRIMARY KEY (`id`),
   ADD KEY `event` (`event`),
   ADD KEY `team` (`team`),
   ADD KEY `user` (`user`) USING BTREE;
 
+--
+-- Indexes for table `team`
+--
 ALTER TABLE `team`
   ADD PRIMARY KEY (`id`),
   ADD KEY `event` (`event`);
 
+--
+-- Indexes for table `user`
+--
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD KEY `event` (`event`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
+--
+-- AUTO_INCREMENT for table `event`
+--
 ALTER TABLE `event`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `score`
+--
 ALTER TABLE `score`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `team`
+--
 ALTER TABLE `team`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `user`
+--
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- Constraints for dumped tables
+--
 
+--
+-- Constraints for table `score`
+--
 ALTER TABLE `score`
   ADD CONSTRAINT `score_ibfk_1` FOREIGN KEY (`event`) REFERENCES `event` (`id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `score_ibfk_2` FOREIGN KEY (`team`) REFERENCES `team` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `score_ibfk_3` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
+--
+-- Constraints for table `team`
+--
 ALTER TABLE `team`
   ADD CONSTRAINT `team_ibfk_1` FOREIGN KEY (`event`) REFERENCES `event` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints for table `user`
+--
 ALTER TABLE `user`
   ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`event`) REFERENCES `event` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
